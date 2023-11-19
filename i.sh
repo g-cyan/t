7444d0ef145e5cbd12b93981b490c452275b016b
fnc2 <68693082+g-cyan@users.noreply.github.com>
2023-11-19 17:23:08 +0900

    Update i.sh

--- i.sh
+++ i.sh
@@ -4,7 +4,7 @@ grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 
 grub-mkconfig -o /boot/grub/grub.cfg
 
-pacman -S grub xfce4 iwd dhcpcd xfce4-terminal thunar pulseaudio pulseaudio-alsa xfce4-goodies lightdm lightdm-gtk-greeter
+pacman -S  networkmanager grub xfce4 iwd dhcpcd xfce4-terminal thunar pulseaudio pulseaudio-alsa xfce4-goodies lightdm lightdm-gtk-greeter
 
 systemctl enable --now lightdm.service
 
