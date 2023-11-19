cca6786c86f7a8bb3c2efd9c1b335c144777115e
fnc2 <68693082+g-cyan@users.noreply.github.com>
2023-11-19 17:17:31 +0900

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,5 +1,15 @@
+pacman -S intel-ucode mesa efibootmgr
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
 pacman -S grub xfce4 iwd dhcpcd xfce4-terminal thunar pulseaudio pulseaudio-alsa xfce4-goodies lightdm lightdm-gtk-greeter
+
 systemctl enable --now lightdm.service
+
 systemctl enable --now NetworkManager
+
 systemctl enable --now iwd
+
 systemctl enable --now dhcpcd.service
